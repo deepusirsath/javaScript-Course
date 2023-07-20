a712739f5182a3873f33d6e359eedfa937a4e590
deepali sirsath <deepali.sirsath1998@gmail.com>
2023-07-20 20:30:51 +0530

    add changes

---
+++ 11-Array/Methods/04.push/01.js
@@ -0,0 +1,9 @@
+// push - it is used to add any element in array in the last position
+
+// The push() method adds zero or more elements to the end of the array.
+
+
+const a = ["apple", "mango", "banana"];
+console.log(a); //["apple", "mango", "banana"]
+a.push("papaya");
+console.log(a); //["apple", "mango", "banana","papaya"]
